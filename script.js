4c054728e3937f33608355f8b1ed99f2e9046b3f
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-25 14:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+document.addEventListener('DOMContentLoaded', function () {
+  // Example usage
+  const str = "Hello World";
+  const subStr = "world";
+  const result = indexOfIgnoreCase(str, subStr);
+  document.getElementById('result').innerText = `Index of '${subStr}' in '${str}': ${result}`;
+});
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+function indexOfIgnoreCase(str, subStr) {
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+
+  return lowerStr.indexOf(lowerSubStr);
+}
